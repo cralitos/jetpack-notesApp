35af2b6de3c84dcb8536280406354cdc64d86004
cralitos <cralitos@gmail.com>
2024-10-18 10:44:13 -0600

    crear navManager

--- app/src/main/java/com/example/notesapp/navigation/NavManager.kt
+++ app/src/main/java/com/example/notesapp/navigation/NavManager.kt
@@ -1,2 +1,24 @@
 package com.example.notesapp.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.notesapp.view.login.TabsView
+import com.example.notesapp.view.notes.HomeView
+import com.example.notesapp.viewmodel.LoginViewModel
+import com.example.notesapp.viewmodel.NotesViewModel
+
+@Composable
+fun NavManager(loginViewModel: LoginViewModel, NotesViewModel: NotesViewModel) {
+    val navController = rememberNavController()
+    NavHost(navController= navController, startDestination = "login") {
+        composable("login") {
+            TabsView(navController = navController, loginViewModel = loginViewModel)
+        }
+        composable("home") {
+            HomeView()
+        }
+    }
+
+}
