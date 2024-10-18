1fc0c6839e4f3a1358f2468996fa29607b91a2cb
cralitos <cralitos@gmail.com>
2024-10-18 10:45:06 -0600

    Invocar NavManager

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -4,23 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.notesapp.navigation.NavManager
 import com.example.notesapp.ui.theme.NotesAppTheme
 import com.example.notesapp.view.login.TabsView
+import com.example.notesapp.viewmodel.LoginViewModel
+import com.example.notesapp.viewmodel.NotesViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val LoginViewModel: LoginViewModel by viewModels()
+        val NotesViewModel: NotesViewModel by viewModels()
+
         setContent {
             NotesAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TabsView(innerPadding)
+                    NavManager(LoginViewModel, NotesViewModel);
                 }
             }
         }
