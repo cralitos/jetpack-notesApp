d2ac63209b4ee42963e696837f43e0fa4f3d82f5
cralitos <cralitos@gmail.com>
2024-10-18 12:03:05 -0600

    funcion logout

--- app/src/main/java/com/example/notesapp/viewmodel/NotesViewModel.kt
+++ app/src/main/java/com/example/notesapp/viewmodel/NotesViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.notesapp.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
 
 class NotesViewModel: ViewModel() {
+    private val auth: FirebaseAuth = Firebase.auth
+
+    fun signOut() {
+        auth.signOut()
+    }
 }
